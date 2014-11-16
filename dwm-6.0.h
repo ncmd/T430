acda39eccffff8f212bfced9854b62de67263ead
appvise <cchong.vise@gmail.com>
2014-11-15 23:41:08 -0800

    Update dwm-6.0.h

--- dwm-6.0.h
+++ dwm-6.0.h
@@ -1,4 +1,4 @@
-#directory = /etc/portage/savedconfig/x11-wm/dwm-6.0.h
+#dir = /etc/portage/savedconfig/x11-wm/dwm-6.0.h
 #ln -s /etc/portage/savedconfig/x11-wm/dwm-6.0 /etc/portage/savedconfig/x11-wm/dwm-6.0.h
 #euse --enable savedconfig
 #emerge dwm
